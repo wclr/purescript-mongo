949c00553628852e2a375f55ff6722bfbb0f5a74
Jesse Nava <code@nava.xyz>
2020-06-22 16:47:22 -0400

    Fix ObjectId instances

--- src/Database/Mongo/ObjectId.purs
+++ src/Database/Mongo/ObjectId.purs
@@ -1,16 +1,13 @@
 module Database.Mongo.ObjectId where
 
-import Prelude
-
-import Foreign (readString, unsafeToForeign)
-import Global.Unsafe (unsafeStringify)
+import Foreign (unsafeToForeign)
 import Simple.JSON (class WriteForeign, class ReadForeign)
 import Unsafe.Coerce (unsafeCoerce)
 
 foreign import data ObjectId :: Type
 
 instance writeForeignObjectId :: WriteForeign ObjectId where
-  writeImpl = unsafeStringify >>> unsafeToForeign
+  writeImpl = unsafeToForeign
 
 instance readForeignObjectId :: ReadForeign ObjectId where
-  readImpl = readString >>> unsafeCoerce
+  readImpl = unsafeCoerce
